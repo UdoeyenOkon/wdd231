05f93625ec3f367eab1cf7bac764ca8da9d61aa3
UdoeyenOkon <uokon2@byupathway.edu>
2026-07-05 19:03:07 +0100

    Home page updates

--- scripts/date.js
+++ scripts/date.js
@@ -2,10 +2,3 @@ const today = new Date();
 const year = document.getElementById("CurrentYear").innerHTML = `${today.getFullYear()}`;
 const day = document.getElementById("LastModified").innerHTML = `${new Intl.DateTimeFormat("en-US", { dateStyle: "full", timeStyle: "medium" }).format(today)}`;
 
-const navbutton = document.querySelector("#ham-button");
-const navLink = document.querySelector("#nav-bar");
-
-navbutton.addEventListener("click", () => {
-    navbutton.classList.toggle("show");
-    navLink.classList.toggle("show");
-});
