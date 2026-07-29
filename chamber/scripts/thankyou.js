9ad72777ca6b711b39aa73379719851a03c6742e
UdoeyenOkon <uokon2@byupathway.edu>
2026-07-29 21:05:53 +0100

    Join Updated

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -13,7 +13,6 @@ localStorage.setItem("numEnquiries-Is", numEnquiries);
 
 
 const formInformation = new URLSearchParams(window.location.search);
-const submissionTime = formInformation.get('timestamp');
 
 document.querySelector("#form-info").innerHTML = `
 <p> <strong>Applicant Name (Title ):</strong> ${formInformation.get('firstName')} ${formInformation.get('lastName')} (${formInformation.get('title')})</p>
@@ -22,6 +21,6 @@ document.querySelector("#form-info").innerHTML = `
 <p><strong>Name of Organization</strong>: ${formInformation.get('organizationName')}</p>
 <p> <strong>Membership Level:</strong> ${formInformation.get('level')}</p>
 <p> <strong>Business / Organization Description:</strong> ${formInformation.get('description')}</p>
-<p> <strong>Registration Requested on:</strong> ${submissionTime}</p>
+<p> <strong>Registration Requested on:</strong> ${formInformation.get('timestamp') }</p>
 `;
 
