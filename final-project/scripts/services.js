49b2e69b2968e64e1be1316d598634d1592bdbb3
UdoeyenOkon <uokon2@byupathway.edu>
2026-08-07 15:10:12 +0100

    Page updates

--- final-project/scripts/services.js
+++ final-project/scripts/services.js
@@ -17,7 +17,7 @@ async function getProfessionalServicesData() {
     catch (error) {
         console.log(error);
     }
-    
+
     createProfessionalServicesCard(professionalServices);
 }
 
@@ -35,7 +35,7 @@ const ServicesLink = document.querySelector("#services");
 ServicesLink.addEventListener("click", () => {
     event.preventDefault();
     document.querySelector("#professional-service-card").innerHTML = "";
-    const Services = professionalServices.Services.filter(service => service.class !== 'Equipment');
+    const Services = professionalServices.filter(service => service.class !== 'Equipment');
     createProfessionalServicesCard(Services);
 });
 
@@ -43,7 +43,7 @@ const Equipmentink = document.querySelector("#equipment");
 Equipmentink.addEventListener("click", () => {
     event.preventDefault();
     document.querySelector("#professional-service-card").innerHTML = "";
-    const Equipment = professionalServices.Services.filter(service => service.class === 'Equipment');
+    const Equipment = professionalServices.filter(service => service.class === 'Equipment');
     createProfessionalServicesCard(Equipment);
 });
 
@@ -52,29 +52,52 @@ function createProfessionalServicesCard(serviceCardToDisplay) {
     serviceCardToDisplay.forEach((x) => {
         let professionalServiceCard = document.createElement("p");
         professionalServiceCard.innerHTML = `${x.type}`
-        professionalServiceCard.addEventListener("click", () => showDetails(x));
+        professionalServiceCard.addEventListener("click", () => showDetailedServices(x));
         document.querySelector("#professional-service-card").appendChild(professionalServiceCard);
     });
 
- };
+};
 
 
 
+function showDetailedServices(x) {
+    const detailsOfServices = document.querySelector("#detailed-services");
+    detailsOfServices.innerHTML = "";
+    detailsOfServices.innerHTML = `
+    <div><h3>${x.type}</h3>
+    <button class="close-button">❌</button></div>
+    <p>${x.description}</p>
+    <p><strong>Professional Fee / Rate</strong>: ${x.feesRates}</p>
+    <p><strong>Available Discount</strong>: ${x.availableDiscount}</p>
+  `;
+    detailsOfServices.showModal();
 
-// function showDetails(course) {
-//     const courseDetails = document.querySelector("#course-details");
-//     courseDetails.innerHTML = "";
-//     courseDetails.innerHTML = `
-//     <button class="close-button">❌</button>
-//     <p>${course.subject} ${course.number}</p>
-//     <h3>${course.title}</h3>
-//     <p><strong>Credits</strong>: ${course.credits}</p>
-//     <p>${course.description}</p>
-//     <p><strong>Certificate</strong>: ${course.certificate}</p>
-//     <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
-//   `;
-//     courseDetails.showModal();
+    let closeModal = document.querySelector(".close-button");
+    closeModal.addEventListener("click", () => detailsOfServices.close());
+}
 
-//     let closeModal = document.querySelector(".close-button");
-//     closeModal.addEventListener("click", () => courseDetails.close());
-// }
+
+let current = Date.now();
+let previousVisit = window.localStorage.getItem("lastVisit");
+localStorage.setItem("lastVisit", current);
+
+let message;
+if (!previousVisit) {
+    message = "You are Welcome!";
+} else {
+    let daysDifference = (current - previousVisit) / 86400000;
+    if (daysDifference < 1) {
+        message = "Back so soon! Awesome!";
+    } else if (daysDifference === 1) {
+        message = "You last visited 1-day ago";
+    } else {
+        message = `You visited ${daysDifference.toFixed(0)} days ago`;
+    }
+}
+
+document.getElementById("visit-message").textContent = message;
+document.getElementById("visitor-info").style.display = "flex";
+
+document.getElementById("close-btn").addEventListener("click", () => {
+    document.getElementById("visitor-info").style.display = "none";
+});
