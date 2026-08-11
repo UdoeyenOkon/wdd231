c56470273d804f857842fa27abc0f1d0116977dd
UdoeyenOkon <uokon2@byupathway.edu>
2026-08-11 08:59:42 +0100

    FINAL project updates

--- final-project/scripts/thankyou.js
+++ final-project/scripts/thankyou.js
@@ -15,13 +15,13 @@ localStorage.setItem("numEnquiries-Is", numEnquiries);
 const formInformation = new URLSearchParams(window.location.search);
 
 document.querySelector("#form-info").innerHTML = `
-<p> <strong>Applicant Name (Title ):</strong> ${formInformation.get('firstName')} ${formInformation.get('lastName')} (${formInformation.get('title')})</p>
+<p> <strong>Client's Name (Title ):</strong> ${formInformation.get('firstName')} ${formInformation.get('lastName')} (${formInformation.get('title')})</p>
 <p> <strong>Email Address:</strong> ${formInformation.get('email')}</p>
 <p> <strong>Telephone Number:</strong> ${formInformation.get('telephone')}</p>
 <p><strong>Name of Organization</strong>: ${formInformation.get('organizationName')}</p>
 <p> <strong>Services Required:</strong> ${formInformation.get('product')}</p>
 <p> <strong>Business / Organization Description:</strong> ${formInformation.get('description')}</p>
-<p> <strong>Service Enquiry Submitted on:</strong> ${formInformation.get('timestamp')}</p>
+<p> <strong>RFI Submitted on:</strong> ${formInformation.get('timestamp')}</p>
 `;
 
 
